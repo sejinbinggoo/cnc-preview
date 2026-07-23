121719b05f74256398e8d922959fd8a7e36aacef
hanho <hanho@DESKTOP-O3HEU6A>
2026-07-23 13:33:41 +0900

    초기 커밋.

---
+++ LatheView/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using Microsoft.Win32;
+using System.IO;
+
+namespace LatheView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.Filter = "NC (*.nc)|*.nc|TXT (*.txt)|*.txt|모든파일|*.*";
+
+            if (dlg.ShowDialog() == true)
+            {
+                txtNC.Text = File.ReadAllText(dlg.FileName);
+            }
+        }
+    }
+}
